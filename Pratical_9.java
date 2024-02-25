a963c06c603ffeecda0bf3d746d3506a748805ae
Adityapithva <121885084+Adityapithva@users.noreply.github.com>
2024-02-25 10:17:58 +0530

    Pratical 9

---
+++ Pratical_9.java
@@ -0,0 +1,13 @@
+class Sample{
+    static int a = 10;
+    static int b = 20;
+    static void add(int a, int b){
+        System.out.println(a+b);
+    }
+}
+public class Pratical_9 {
+    public static void main(String[] args) {
+        Sample.add(10, 20);
+        System.out.println(Sample.a + Sample.b);
+    }
+}
