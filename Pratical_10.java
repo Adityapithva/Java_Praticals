5373076145d003427d873ee49ee2e112ccc2e732
Adityapithva <121885084+Adityapithva@users.noreply.github.com>
2024-03-12 10:37:34 +0530

    Pratical 10

---
+++ Pratical_10.java
@@ -0,0 +1,8 @@
+public class Pratical_10 {
+    public static void main(String[] args) {
+        final int count = 0;
+        System.out.print(count);
+        count++;
+        System.out.print(count);
+    }
+}
