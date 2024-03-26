3c400b30e116044857ad0ba3dd1b6450abddac46
Adityapithva <121885084+Adityapithva@users.noreply.github.com>
2024-03-26 10:25:41 +0530

    Pratical 16

---
+++ Pratical_16.java
@@ -0,0 +1,28 @@
+class Animals {
+    public void eats() {
+        System.out.println("Animal is eating");
+    }
+}
+
+class Dog extends Animals {
+    public void barks() {
+        System.out.println("Dog is barking");
+    }
+}
+
+class Cat extends Animals {
+    public void meows() {
+        System.out.println("Cat is meowing");
+    }
+}
+
+public class Pratical_16 {
+    public static void main(String[] args) {
+        Dog obj1 = new Dog();
+        Cat obj2 = new Cat();
+        obj1.eats(); // Call method from Animals class
+        obj1.barks();
+        obj2.eats(); // Call method from Animals class
+        obj2.meows();
+    }
+}
